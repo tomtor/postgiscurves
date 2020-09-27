370e621f8eeb402a890016f6c16f1f9f7cec8560
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-09-27 09:32:37 +0000

    update

--- load/load-bag.sh
+++ load/load-bag.sh
@@ -12,10 +12,14 @@ do
   TAB=$(echo "$f" | sed -e "s/9999PND//" -e "s/\\.xml.*//")
   echo $TAB starts at $(date)
 
-  ogr2ogr -progress -f "PostgreSQL" -where "'tijdvakgeldigheid|einddatumTijdvakGeldigheid' IS NULL AND 'tijdvakgeldigheid|aanduidingRecordInactief' <> 'J'" -a_srs EPSG:28992 -nlt CONVERT_TO_LINEAR -lco SPATIAL_INDEX=GIST -lco DIM=2 PG:"dbname=$DB host=$HOST port=$PORT" GMLAS:"$f"
+  ogr2ogr -progress -f "PostgreSQL" -a_srs EPSG:28992 -nlt CONVERT_TO_LINEAR -lco SPATIAL_INDEX=GIST -lco DIM=2 PG:"dbname=$DB host=$HOST port=$PORT" GMLAS:"$f"
 
   echo $TAB ends at $(date)
 
 done
+
+echo "select count(*) from bag_extract_deelbestand__antwoord_producten_lvc_product_pand;" | psql -U $PGU -h $HOST -p $PORT $DB
+
+echo "delete from bag_extract_deelbestand__antwoord_producten_lvc_product_pand where tijdvakgeldigheid_einddatumtijdvakgeldigheid IS NOT NULL or aanduidingrecordinactief::text = 'J';" | psql -U $PGU -h $HOST -p $PORT $DB
 
 echo "delete from bag_extract_deelbestand__antwoord_producten_lvc_product_pand where not st_isvalid(pandgeometrie);" | psql -U $PGU -h $HOST -p $PORT $DB
