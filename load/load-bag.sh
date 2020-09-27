3a5510351643ce005b8385e835755d684043b538
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-09-27 06:20:56 +0000

    update bag load

--- load/load-bag.sh
+++ load/load-bag.sh
@@ -3,7 +3,7 @@ HOST=localhost
 PGU=tom
 DB=bgt
 
-echo "select 'drop table '||tablename||';' cascade from pg_tables where tablename like 'bag_%'" | \
+echo "select 'drop table '||tablename||' cascade;' from pg_tables where tablename like 'bag_%'" | \
     psql -U $PGU -h $HOST -p $PORT -d $DB -t | \
     psql -U $PGU -h $HOST -p $PORT -d $DB
 
