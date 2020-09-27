06ccd7bf32c836c64c810251613521c6925a1ca2
Tom Vijlbrief <tvijlbrief@gmail.com>
2020-09-27 12:23:40 +0000

    update

--- load/diff-view.sql
+++ load/diff-view.sql
@@ -1,6 +1,6 @@
 -- View: public.diffbag
 
--- DROP VIEW public.diffbag;
+DROP VIEW public.diffbag cascade;
 
 CREATE OR REPLACE VIEW public.diffbag
  AS
@@ -31,21 +31,22 @@ CREATE OR REPLACE VIEW public.diffbag
     a."nummeraanduidingreeks_3.identificatiebagvbolaagstehuisnummer",
     a."nummeraanduidingreeks_3.identificatiebagvbohoogstehuisnummer",
     a.plus_status,
-    a.wkb_geometry
+    a.wkb_geometry,
+    b.identificatie as bag_match_identificatie
    FROM pand a
-     JOIN bag_extract_deelbestand__antwoord_producten_lvc_product_pand b ON st_overlaps(a.wkb_geometry, b.pandgeometrie)
-  WHERE a.eindregistratie IS NULL AND b.tijdvakgeldigheid_einddatumtijdvakgeldigheid IS NULL AND b.aanduidingrecordinactief::text <> 'J'::text AND abs(st_perimeter(a.wkb_geometry) - st_perimeter(b.pandgeometrie)) > 1::double precision;
+     -- JOIN bag_extract_deelbestand__antwoord_producten_lvc_product_pand b ON st_overlaps(st_snaptogrid(a.wkb_geometry,0.1), st_snaptogrid(b.pandgeometrie,0.1))
+     JOIN bag_extract_deelbestand__antwoord_producten_lvc_product_pand b ON st_overlaps(a.wkb_geometry,b.pandgeometrie)
+  -- WHERE a.eindregistratie IS NULL AND b.tijdvakgeldigheid_einddatumtijdvakgeldigheid IS NULL AND b.aanduidingrecordinactief::text <> 'J'::text AND abs(st_perimeter(a.wkb_geometry) - st_perimeter(b.pandgeometrie)) > 1::double precision;
+  WHERE abs(st_perimeter(a.wkb_geometry) - st_perimeter(b.pandgeometrie)) > 1::double precision;
 
 -- ALTER TABLE public.diffbag OWNER TO postgresadmin;
 
 -- GRANT ALL ON TABLE public.diffbag TO postgresadmin;
--- GRANT ALL ON TABLE public.diffbag TO PUBLIC;
+GRANT ALL ON TABLE public.diffbag TO PUBLIC;
 
 
 create materialized view diffbagmat as select * from diffbag;
 
-CREATE INDEX diffbagmat_wkb_geometry_geom_idx
-    ON public.diffbagmat USING gist (wkb_geometry) TABLESPACE pg_default;
+CREATE INDEX diffbagmat_wkb_geometry_geom_idx ON public.diffbagmat USING gist (wkb_geometry) TABLESPACE pg_default;
 
-CREATE INDEX diff_ogc_id ON public.diffbagmat USING btree
-    (ogc_fid ASC NULLS LAST) INCLUDE(ogc_fid) TABLESPACE pg_default;
+CREATE INDEX diff_ogc_id ON public.diffbagmat USING btree (ogc_fid ASC NULLS LAST) INCLUDE(ogc_fid) TABLESPACE pg_default;
